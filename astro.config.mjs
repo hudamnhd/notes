a3d4946d42cdedc952d014b7d8c337e0e02e8cfe
hudamnhd <muhamadhuda519@gmail.com>
2025-04-08 00:12:48 +0700

    fix error build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://mhda.netlify.app",
-  integrations: [mdx(), sitemap()],
+	site: "https://mhda.netlify.app",
+	integrations: [mdx(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
-  markdown: {
-    shikiConfig: {
-      theme: "github-dark",
-    },
-  },
-  redirects: {
-    "/posts": "/",
-  },
-  trailingSlash: "never",
+	markdown: {
+		shikiConfig: {
+			theme: "github-dark",
+		},
+	},
+	redirects: {
+		"/posts": "/",
+	},
+	trailingSlash: "never",
+	image: {
+		service: passthroughImageService(),
+	},
 });
